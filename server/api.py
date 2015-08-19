41e796027327b8347725f1c69f9158007df6d027
Alex Vykaliuk <alekzvik@gmail.com>
2015-08-19 19:39:23 +0300

    Search returns randomly generated data with right signature

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+import random
+import string
 from flask import Blueprint, current_app, jsonify
 
 
@@ -11,6 +12,18 @@ def data_path(filename):
     return u"%s/%s" % (data_path, filename)
 
 
+def generate_dummy_product_data():
+    return {
+        'shop': {
+            'lat': 59 + random.random(),
+            'lng': 18 + random.random(),
+            'name': ''.join(random.sample(string.ascii_letters, 7))},
+        'popularity': random.random(),
+        'name': ''.join(random.sample(string.ascii_letters, 8)),
+    }
+
+
 @api.route('/search', methods=['GET'])
 def search():
-    return jsonify({'products': []})
+    products = [generate_dummy_product_data() for _ in range(25)]
+    return jsonify({'products': products[:20]})
