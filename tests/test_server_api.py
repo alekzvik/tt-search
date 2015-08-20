b05634f870e50cc1a7f97fb89dd97628cfcc3e33
Alex Vykaliuk <alekzvik@gmail.com>
2015-08-20 12:21:28 +0300

    Added test for products count in search

--- tests/test_server_api.py
+++ tests/test_server_api.py
@@ -6,13 +6,13 @@ from server.api import parse_products, parse_shops, parse_csv_files
 
 class TestSearchAPI(object):
     def test_returns_20_products(self, get):
-        response = get('search')
+        response = get('/search')
         assert response.status_code == 200
         assert 'products' in response.json
         assert len(response.json['products']) == 20
 
     def test_product_signature(self, get):
-        response = get('search')
+        response = get('/search')
         product = response.json['products'][0]
         assert 'popularity' in product.keys()
         assert 'shop' in product.keys()
@@ -33,8 +33,12 @@ class TestSearchAPI(object):
     def test_csv_parsing_is_done_before_first_request(self, app):
         assert parse_csv_files in app.before_first_request_funcs
 
+    def test_search_products_count(self, get):
+        response = get('/search?count=35')
+        assert len(response.json['products']) == 35
+
     def test_parse_csv_data(self, monkeypatch):
-        products_mock = Mock()
+        products_mock = Mock(return_value=())
         shops_mock = Mock(return_value=(None, None))
         monkeypatch.setattr('server.api.parse_products', products_mock)
         monkeypatch.setattr('server.api.parse_shops', shops_mock)
